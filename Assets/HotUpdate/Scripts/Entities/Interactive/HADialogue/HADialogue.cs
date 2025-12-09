2dc2549120b8575d796557ada9e00b1f3f5e7f58
TenMilesAway <zll749486310@163.com>
2025-12-09 22:07:03 +0800

    【12/9】可交互物体简单逻辑测试

---
+++ Assets/HotUpdate/Scripts/Entities/Interactive/HADialogue/HADialogue.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cyber;
+
+namespace HA
+{
+    /// <summary>
+    /// 此类区别于 DSDialogue, 隔离出来的原因是为了统一接口管理
+    /// </summary>
+    public class HADialogue : MonoBehaviour, IDialogue
+    {
+        // 对话 SO
+        [SerializeField] private DSDialogueContainerSO _dialogueContainer; // 对话 Graph
+        [SerializeField] private DSDialogueGroupSO _dialogueGroup;         // 对话 Group
+        [SerializeField] private DSDialogueSO _dialogue;                   // 对话节点
+        [SerializeField] private bool _groupedDialogues;                   // 是否仅使用分组对话
+        [SerializeField] private bool _startingDialogueOnly;               // 是否仅使用开始对话节点
+        [SerializeField] private int _selectedDialogueGroupIndex;          // 分组对话索引
+        [SerializeField] private int _selectedDialogueIndex;               // 对话索引
+        
+        // 内部变量
+        private const string _interactionPrompt = "按 F 进行对话"; // 交互提示语
+        private bool _isInteractable;                             // 是否可交互
+
+        /// 供外部调用
+        [HideInInspector] public string InteractionPrompt => _interactionPrompt;
+        [HideInInspector] public bool IsInteractable => _isInteractable;
+
+        /// <summary>
+        /// 交互：对话
+        /// </summary>
+        public void Interact(object interactor)
+        {
+            // 对话
+            HADebug.LogFormat("开始对话 [{0}]", gameObject.name);
+        }
+
+        #region 接口预留字段
+        public string DialogueID => throw new System.NotImplementedException();
+
+        public Vector3 Position => throw new System.NotImplementedException();
+
+        public void StartDialogue()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Assets/HotUpdate/Scripts/Interactive/PlayerInteractiveTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HA
+{
+    public class PlayerInteractiveTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private float _interactiveRadius = 2f;
+        [SerializeField]
+        private LayerMask _interactiveLayer;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out IInteractive interactiveObject))
+            {
+                InteractiveDataManager.GetInstance().AddInteractive(interactiveObject);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent(out IInteractive interactiveObject))
+            {
+                InteractiveDataManager.GetInstance().RemoveInteractive(interactiveObject);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            // 设置颜色为黄色，表示交互范围
+            Gizmos.color = new Color(1f, 1f, 0f, 0.5f);
+            // 在玩家位置绘制球体
+            Gizmos.DrawSphere(transform.position, _interactiveRadius);
+        }
+    }
+}
